ab301dbbd15483b3cd7526338a5b3f89a6bc0d44
Swaforde <swaforde@gmail.com>
2021-05-30 21:21:46 +0200

    adding script

---
+++ Assets/Personnage/MoveBehaviour.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class MoveBehaviour : GenericBehaviour
+{
+	public float runSpeed = 1.0f;
+	public Animator anim;
+
+	void Start()
+	{
+		behaviourManager.SubscribeBehaviour(this);
+		behaviourManager.RegisterDefaultBehaviour(this.behaviourCode);
+		anim = gameObject.GetComponent<Animator>();
+	}
+
+	void Update()
+	{
+	}
+	public override void LocalFixedUpdate()
+	{
+		MovementManagement(behaviourManager.GetH, behaviourManager.GetV);
+
+		JumpManagement();
+	}
+
+	void JumpManagement()
+	{
+
+	}
+
+	void MovementManagement(float horizontal, float vertical)
+	{
+		Rotating(horizontal, vertical);
+
+		if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S)){
+			transform.Translate(0, 0, 0.3f);
+			anim.SetBool("walk", true);
+		}else{
+			anim.SetBool("walk", false);
+		}
+	}
+
+	Vector3 Rotating(float horizontal, float vertical)
+	{
+		Vector3 forward = behaviourManager.playerCamera.TransformDirection(Vector3.forward);
+
+		forward.y = 0.0f;
+		forward = forward.normalized;
+
+		Vector3 right = new Vector3(forward.z, 0, -forward.x);
+		Vector3 targetDirection;
+		targetDirection = forward * vertical + right * horizontal;
+
+		if ((behaviourManager.IsMoving() && targetDirection != Vector3.zero))
+		{
+			Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+
+			Quaternion newRotation = Quaternion.Slerp(behaviourManager.GetRigidBody.rotation, targetRotation, behaviourManager.turnSmoothing);
+			behaviourManager.GetRigidBody.MoveRotation(newRotation);
+			behaviourManager.SetLastDirection(targetDirection);
+		}
+		if (!(Mathf.Abs(horizontal) > 0.9 || Mathf.Abs(vertical) > 0.9))
+		{
+			behaviourManager.Repositioning();
+		}
+
+		return targetDirection;
+	}
+}
